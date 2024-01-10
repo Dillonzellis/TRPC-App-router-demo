44ec88a641e20a51c87380fbc10e2dcff85ad653
dillonzellis <dillonzellis@gmail.com>
2024-01-10 12:34:39 -0500

    set up trpc and drizzle

---
+++ src/server/index.ts
@@ -0,0 +1,22 @@
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import Database from "better-sqlite3";
+import { z } from "zod";
+
+import { publicProcedure, router } from "./trpc";
+
+import { todos } from "@/db/schema";
+
+const sqlite = new Database("sqlite.db");
+const db = drizzle(sqlite);
+
+migrate(db, { migrationsFolder: "drizzle" });
+
+export const appRouter = router({
+  getTodos: publicProcedure.query(async () => {
+    return await db.select().from(todos).all();
+  }),
+});
+
+export type AppRouter = typeof appRouter;
